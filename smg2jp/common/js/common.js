da59edcd39ae4e62f6ec0a6a94a21b76b6e96c76
Kiz3r333 <smgcometobservatory@gmail.com>
2022-01-31 18:33:37 +0000

    Partial SMG JP Website, Ruffle Test

---
+++ smg2jp/common/js/common.js
@@ -0,0 +1,36 @@
+
+
+function initRollovers() {
+	if (!document.getElementById) return
+	
+	var aPreLoad = new Array();
+	var sTempSrc;
+	var aImages = document.getElementsByTagName('img');
+
+	for (var i = 0; i < aImages.length; i++) {		
+		if (aImages[i].className == 'hover') {
+			var src = aImages[i].getAttribute('src');
+			var ftype = src.substring(src.lastIndexOf('.'), src.length);
+			var hsrc = src.replace(ftype, '_on'+ftype);
+
+			aImages[i].setAttribute('hsrc', hsrc);
+			
+			aPreLoad[i] = new Image();
+			aPreLoad[i].src = hsrc;
+			
+			aImages[i].onmouseover = function() {
+				sTempSrc = this.getAttribute('src');
+				this.setAttribute('src', this.getAttribute('hsrc'));
+			}	
+			
+			aImages[i].onmouseout = function() {
+				if (!sTempSrc) sTempSrc = this.getAttribute('src').replace('_on'+ftype, ftype);
+				this.setAttribute('src', sTempSrc);
+			}
+		}
+	}
+}
+
+window.onload = initRollovers;
+
+
---
+++ smg2jp/common/js/opewin.js
@@ -0,0 +1,3 @@
+//↓開くファイルのみ引数で渡す
+
+function opeWin(f){ winName1 = window.open(f,'winName1','WIDTH=660,HEIGHT=440,SCROLLBARS=1'); winName1.focus(); }
